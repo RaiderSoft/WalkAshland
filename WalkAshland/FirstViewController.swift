747c146285c6a404aba21c750dd89a89c769469d
Faisala2019 <faisala@sou.edu>
2020-03-25 00:37:22 -0700

    Initial Commit

---
+++ WalkAshland/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WalkAshland
+//
+//  Created by Faisal Alik on 3/25/20.
+//  Copyright © 2020 RaiderSoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WalkAshland/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WalkAshland
+//
+//  Created by Faisal Alik on 3/25/20.
+//  Copyright © 2020 RaiderSoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
