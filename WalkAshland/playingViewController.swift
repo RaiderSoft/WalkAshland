1316112c2e0501a234b43140d271988a8f8e56e7
Dylan <Pittsd@sou.edu>
2020-04-13 16:22:07 -0700

    Updated Play screen

--- WalkAshland/playingViewController.swift
+++ WalkAshland/playingViewController.swift
@@ -9,37 +9,70 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import AVFoundation
 
 class playingViewController: UIViewController {
     //This is the initial starting point for the tour
     //var placesClient: GMSPlacesClient!
     
+    //Created by Dylan
+    //The audio player
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let camera = GMSCameraPosition.camera(withLatitude: 42.1972 , longitude: 122.7153, zoom: 9.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        
+        let mapView = GMSMapView.map(withFrame: self.ViewForMap.frame, camera: camera)
         self.view.addSubview(mapView)
-        
-        //Allow corrent location accessing
-        
-        
         //placesClient = GMSPlacesClient.shared()
-        
-        
+
+
         let position = CLLocationCoordinate2D(latitude: 42.1972, longitude: 122.7153)
-        
+
         let marker = GMSMarker(position: position)
         marker.title = "HELLO WORLD"
         marker.map = mapView
-        
+
         let marker2 = GMSMarker(position: CLLocationCoordinate2D(latitude: 42.5, longitude: 122.7153))
         marker2.title = "Hll"
         marker2.map = mapView
         
+       //Created by Dylan
+       //bring mediabar forward
+       MediaBar.layer.zPosition = 1;
+       
+       //Do - try - catch audio player for file sample.wav
+       do {
+           audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "wav")!))
+           
+       } catch {
+           print(error)
+       }
+    
     }
     
+    //Created By Dylan
+    //audio player outlets and actions
+    //media tool bar
+    @IBOutlet weak var MediaBar: UIToolbar!
+
+    //play
+    @IBAction func Play(_ sender: Any) {
+        audioPlayer.play()
+    }
+    //pause
+    @IBAction func Pause(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        }
+    }
+    //rewind
+    @IBAction func Rewind(_ sender: Any) {
+    }
+    //fastforward
+    @IBAction func FastForward(_ sender: Any) {
+    }
     
+    @IBOutlet weak var ViewForMap: UIView!
 }
