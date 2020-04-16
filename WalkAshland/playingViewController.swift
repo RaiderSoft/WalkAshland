7c66313766fdffc711f422b71946b83a38464510
Dylan <Pittsd@sou.edu>
2020-04-16 16:17:59 -0700

    Playing screen Update (drop down Menu added)

--- WalkAshland/playingViewController.swift
+++ WalkAshland/playingViewController.swift
@@ -11,13 +11,18 @@ import GoogleMaps
 import GooglePlaces
 import AVFoundation
 
-class playingViewController: UIViewController {
+class playingViewController: UIViewController,
+    UIPickerViewDataSource, UIPickerViewDelegate{
+    
     //This is the initial starting point for the tour
     //var placesClient: GMSPlacesClient!
     
     //Created by Dylan
     //The audio player
     var audioPlayer = AVAudioPlayer()
+    
+    //List Text for drop down(PickerTextView)
+    var audioList: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +32,7 @@ class playingViewController: UIViewController {
         self.view.addSubview(mapView)
         //placesClient = GMSPlacesClient.shared()
 
-        
+
        //Created by Dylan
        //bring mediabar forward
        MediaBar.layer.zPosition = 1;
@@ -39,9 +44,41 @@ class playingViewController: UIViewController {
        } catch {
            print(error)
        }
-    
+        
+        //mapView.settings.setAllGesturesEnabled(false)
+        //used for Drop down menu(PickerTextView)
+        self.PickerTextView.delegate = self
+        self.PickerTextView.dataSource = self
+        
+        //text for Drop down menu(PickerTextView)
+        audioList = ["Track 1", "Track 2", "Track 3"]
+
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //number of components in the drop down(PickerTextView)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //number of rows for drop down(PickerTextView)
+    //equal to the amount of elements in audiolist
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return audioList.count
+    }
+    
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        PickerTextView.text = audioList[row]
+//    }
+    
+    //Names of the elements in drop down(PickerTextView) equal to elements in audioList
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return audioList[row]
+    }
     //Created By Dylan
     //audio player outlets and actions
     //media tool bar
@@ -62,6 +99,22 @@ class playingViewController: UIViewController {
     }
     //fastforward
     @IBAction func FastForward(_ sender: Any) {
+    }
+    
+    //Drop Down Menu
+    @IBOutlet weak var PickerTextView: UIPickerView!
+    
+    //Action For displaying Drop Down Menu
+    @IBAction func DisplayList(_ sender: Any) {
+                
+        //if enable disable and if disable enable on click
+        if PickerTextView.layer.zPosition == 0 {
+            PickerTextView.isUserInteractionEnabled = true
+            PickerTextView.layer.zPosition = 1
+        } else {
+            PickerTextView.isUserInteractionEnabled = false
+            PickerTextView.layer.zPosition = 0
+        }
     }
     
     @IBOutlet weak var ViewForMap: UIView!
